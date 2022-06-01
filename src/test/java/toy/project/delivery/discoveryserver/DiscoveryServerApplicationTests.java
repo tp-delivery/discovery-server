dd5f18c70b25df65861f161cac590637aab08a67
paki1019 <paki1019@gmail.com>
2022-06-01 14:53:14 +0900

    boilerplate

---
+++ src/test/java/toy/project/delivery/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.project.delivery.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
